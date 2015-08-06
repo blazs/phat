a3b6636efd9de1223796057e062f0f994ca7e34a
Blaz Sovdat <blaz.sovdat@gmail.com>
2015-08-06 09:30:24 +0200

    Can now compute persistent relative homology

--- include/phat/compute_persistence_pairs.h
+++ include/phat/compute_persistence_pairs.h
@@ -24,6 +24,71 @@
 #include <phat/algorithms/twist_reduction.h>
 
 namespace phat {
+    // Extracts persistence pairs in separate dimensions from a reduced
+    // boundary matrix representing ``double`` filtration. The pairs
+    // give persistent relative homology of the pair of filtrations.
+    // TODO: Use it with standard reduction algorithm (no template option).
+    template< typename ReductionAlgorithm, typename Representation >
+    void compute_relative_persistence_pairs(std::vector<persistence_pairs>& pairs, boundary_matrix<Representation>& boundary_matrix, const std::map<int, int>& L) {
+        ReductionAlgorithm reduce;
+        reduce(boundary_matrix);
+        std::map<int, bool> free;
+        std::map<int, int> invL;
+        for (std::map<int, int>::const_iterator it = L.begin(); it != L.end(); ++it) { invL[it->second] = it->first; }
+        for (std::vector<persistence_pairs>::iterator it = pairs.begin(); it != pairs.end(); ++it) { it->clear(); }
+        for (index idx = 0; idx < boundary_matrix.get_num_cols(); ++idx) {
+            int dimension = boundary_matrix.get_dim(idx);
+            if (L.find(idx) != L.end()) { ++dimension; }
+            free[idx] = true;
+            if (!boundary_matrix.is_empty(idx)) {
+                index birth = boundary_matrix.get_max_index(idx);
+                index death = idx;
+                pairs[dimension-1].append_pair(birth, death);
+                free[birth] = false;
+                free[death] = false;
+            } else {
+                // This is an L-simplex and a (dimension+1)-dimensional cycle
+                if (L.find(idx) != L.end()) {
+                    pairs[dimension].append_pair(idx, -1);
+                }
+            }
+        }
+        for (std::map<int, bool>::iterator it = free.begin(); it != free.end(); ++it) {
+            if (it->second) {
+                int dimension = boundary_matrix.get_dim(it->first);
+                if (invL.find(it->first) == invL.end() && L.find(it->first) == L.end()) {
+                    pairs[dimension].append_pair(it->first, -1);
+                }
+            }
+        }
+    }
+
+    // Extracts persistence pairs in separate dimensions; expects a d-dimensional vector of persistent_pairs
+    template< typename ReductionAlgorithm, typename Representation >
+    void compute_persistence_pairs(std::vector<persistence_pairs>& pairs, boundary_matrix<Representation>& boundary_matrix) {
+        ReductionAlgorithm reduce;
+        reduce(boundary_matrix);
+        std::map<int, bool> free;
+        for (std::vector<persistence_pairs>::iterator it = pairs.begin(); it != pairs.end(); ++it) { it->clear(); }
+        for (index idx = 0; idx < boundary_matrix.get_num_cols(); ++idx) {
+            int dimension = boundary_matrix.get_dim(idx);
+            free[idx] = true;
+            if (!boundary_matrix.is_empty(idx)) {
+                index birth = boundary_matrix.get_max_index(idx);
+                index death = idx;
+                pairs[dimension-1].append_pair(birth, death);
+                // Cannot be of the form (a, infinity)
+                free[birth] = false;
+                free[death] = false;
+            }
+        }
+        for (std::map<int, bool>::iterator it = free.begin(); it != free.end(); ++it) {
+            if (it->second) {
+                int dimension = boundary_matrix.get_dim(it->first);
+                pairs[dimension].append_pair(it->first, -1);
+            }
+        }
+    }
 
     template< typename ReductionAlgorithm, typename Representation >
     void compute_persistence_pairs( persistence_pairs& pairs, boundary_matrix< Representation >& boundary_matrix ) {
